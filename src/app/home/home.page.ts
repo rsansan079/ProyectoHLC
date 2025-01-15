adc57ab7eb42a15e8e5475c48a43e204bdeff5ee
rsansan079 <rsansan079@g.educaand.es>
2025-01-15 14:13:27 +0100

    Parte 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../tarea';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tareaEditando: Tarea;
+  arrayColeccionTareas: any = [{
+    id: "",
+    data: {} as Tarea
 
+  }];
+
+
+
+
+  constructor(private firestoreService: FirestoreService) {
+    // Crear una tarea vacía al empezar
+    this.tareaEditando = {} as Tarea;
+
+
+
+    this.obtenerListaTareas();
+  }
+
+
+
+
+  clickBotonInsertar() {
+    this.firestoreService.insertar("tareas", this.tareaEditando)
+      .then(() => {
+        console.log('Tarea creada correctamente');
+        //Limpiar el contenido de la tarea que se está editando
+        this.tareaEditando = {} as Tarea;
+      }, (error) => {
+        console.error(error);
+      });
+  }
+
+
+
+  obtenerListaTareas() {
+    this.firestoreService.consultar("tareas").subscribe((resultadoConsultaTareas) => {
+
+      this.arrayColeccionTareas = [];
+      resultadoConsultaTareas.forEach((datosTarea: any) => {
+        this.arrayColeccionTareas.push({
+          id: datosTarea.payload.doc.id,
+          data: datosTarea.payload.doc.data()
+        })
+      })
+    }
+    )
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCYDpkRvB7DRWRJ-J444nHAY-mfwCrhz80",
+    authDomain: "proyecto-hlc-5ce67.firebaseapp.com",
+    projectId: "proyecto-hlc-5ce67",
+    storageBucket: "proyecto-hlc-5ce67.firebasestorage.app",
+    messagingSenderId: "861296493344",
+    appId: "1:861296493344:web:d51734ddbcb18f01020d45"
+  }
 };
 
 /*
